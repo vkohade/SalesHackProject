087a506e1a8d022164e6265bb908dafa4fd9397d
Vaibhav Kohade <vkohade@microsoft.com>
2024-01-10 12:46:27 +0530

    Removed hard codings for Kusto

--- src/popup.js
+++ src/popup.js
@@ -11,26 +11,25 @@ let input_query = document.getElementById("query");
 let flyoutMenu = document.getElementsByClassName("dropdown-content");
 let flyoutMenuItems;
 
-let kusto_suffix = "&endpoint=https://powerappsclientneu.northeurope.kusto.windows.net";
-let kusto_prefix = "https://portal.microsoftgeneva.com/logs/kusto?database=0b14a44360bf4cae8e1e090ac91a04e5&query=";
-let configData = {
-  "SalesForecasting": "cluster('crmanacus.kusto.windows.net').database('CRMProdCloudServices').SFTraceEvent | union cluster('crmanaweu.kusto.windows.net').database('CRMProdCloudServices').SFTraceEvent | where TIMESTAMP > ago(1d) | where ActivityId == $(Guid) or  OrgId == $(Guid)",
-  "Sales Forecasting Client Side" : "cluster('crmanacus.kusto.windows.net').database('CRMProdCloudServices').SFTraceEvent | union cluster('crmanaweu.kusto.windows.net').database('CRMProdCloudServices').SFTraceEvent | where TIMESTAMP > ago(1d) | where ActivityId == $(Guid) or  OrgId == $(Guid)"};
-
+let kusto_suffix =
+  "&endpoint=https://powerappsclientneu.northeurope.kusto.windows.net";
+let kusto_prefix =
+  "https://portal.microsoftgeneva.com/logs/kusto?database=0b14a44360bf4cae8e1e090ac91a04e5&query=";
 let adoBaseUrl;
 let kustoBaseUrl;
 let unifyBaseUrl = "https://unify.services.dynamics.com/CRM/Org";
 let YOUR_API_KEY = "";
-
-
+let configJson = {};
+let globalItems;
 
 chrome.storage.sync.get(
   { adoSettings: {}, kustoSettings: {}, newTab: true, openAISettings: {} },
   function (items) {
+    globalItems = items;
     console.log(JSON.stringify(items));
     let companyName = items.adoSettings.company;
     let projectName = items.adoSettings.project;
-    let configJson = items.kustoSettings.configJson;
+    configJson = JSON.parse(items.kustoSettings.configJson);
 
     // make sure they're both present
     if (
@@ -46,41 +45,38 @@ chrome.storage.sync.get(
     form_newTab.checked = items.newTab;
     adoBaseUrl = `https://dev.azure.com/${companyName}/${projectName}`;
     YOUR_API_KEY = items.openAISettings.apiKey;
+
+    // Create a new link for each kusto scenario and add it to the flyout menu
+    for (const [key, value] of Object.entries(configJson)) {
+      var link = document.createElement("div");
+      link.className = "dropdown-items";
+      link.id = key;
+      link.innerHTML = key;
+      flyoutMenu[0].appendChild(link);
+    }
+
+    flyoutMenuItems = document.getElementsByClassName("dropdown-items");
+
+    Array.from(flyoutMenuItems).forEach((element) => {
+      element.addEventListener("click", () => {
+        let finalKustoQuery =
+          kustoBaseUrl + configJson[element.id] + kusto_suffix;
+        console.log(finalKustoQuery);
+        window.open(finalKustoQuery, "_blank");
+      });
+    });
   }
 );
 
-var replacer = function(tpl, data) {
-	var re = /\$\(([^\)]+)?\)/g, match;
-  while(match = re.exec(tpl)) {
-		tpl = tpl.replace(match[0], data[match[1]])
+var replacer = function (tpl, data) {
+  var re = /\$\(([^\)]+)?\)/g,
+    match;
+  while ((match = re.exec(tpl))) {
+    tpl = tpl.replace(match[0], data[match[1]]);
     re.lastIndex = 0;
   }
   return tpl;
-}
-
-
-// Create a new link for each kusto scenario and add it to the flyout menu
-for (const [key, value] of Object.entries(configData)) {
-  var link = document.createElement("div");
-  link.className = "dropdown-items";
-  link.id = key;
-  link.innerHTML = key;
-  flyoutMenu[0].appendChild(link);
-}
-
-
-flyoutMenuItems = document.getElementsByClassName("dropdown-items");
-
-
-Array.from(flyoutMenuItems).forEach(element => {
-  element.addEventListener('click', () => {
-    let finalKustoQuery = kustoBaseUrl + configData[element.id] + kusto_suffix;
-    console.log(finalKustoQuery);
-    window.open(finalKustoQuery, '_blank');
-  });
-});
-
-
+};
 
 // setup triggers
 button_settings.onclick = function () {
@@ -102,7 +98,6 @@ unify_search.onclick = function () {
   createNewTab(`${unifyBaseUrl}/${search}`);
 };
 
-
 document.addEventListener("DOMContentLoaded", function () {
   chrome.tabs.query({ active: true, currentWindow: true }).then((resp) => {
     const [tab] = resp;
@@ -119,15 +114,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
           if (isSelectedTextGuid(result)) {
             unify_search.style.display = "block";
-            //kusto_search.style.display = "block";
+            kusto_search.style.display = "block";
 
-            for (const [key, value] of Object.entries(configData)) {
-              configData[key] = replacer(value, { "Guid": `"${input_query.value}"` });
+            for (const [key, value] of Object.entries(configJson)) {
+              configJson[key] = replacer(value, {
+                Guid: `"${input_query.value}"`,
+              });
             }
             kustoBaseUrl = `${kusto_prefix}`; //${configData}${kusto_suffix}
           } else {
             unify_search.style.display = "none";
-            //kusto_search.style.display = "none";
+            kusto_search.style.display = "none";
           }
         });
       return;
@@ -152,17 +149,18 @@ function askGPT() {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${YOUR_API_KEY}`,
+      Authorization: `Bearer ${YOUR_API_KEY}`,
     },
     body: JSON.stringify({
-      "model": "text-davinci-003",
-      "prompt": "I am a highly intelligent question answering bot. If you ask me a  Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
-      "temperature": 0,
-      "max_tokens": 100,
-      "top_p": 1,
-      "frequency_penalty": 0.0,
-      "presence_penalty": 0.0,
-      "stop": ["\n"]
+      model: "text-davinci-003",
+      prompt:
+        "I am a highly intelligent question answering bot. If you ask me a  Spain.\n\nQ: How many squigs are in a bonk?\nA: Unknown\n\nQ: Where is the Valley of Kings?\nA:",
+      temperature: 0,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      stop: ["\n"],
     }),
   });
 
